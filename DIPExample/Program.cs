e126191044e931a71342a91458a6647876682be5
Sohil007 <sohil.mansuri@in.musafir.com>
2024-10-11 11:20:53 +0530

    check added to change logging destination

--- DIPExample/Program.cs
+++ DIPExample/Program.cs
@@ -7,14 +7,27 @@ namespace DIPExample
         static void Main(string[] args)
         {
             
-            IUnityContainer container = new UnityContainer();
-            container.RegisterType<ILogger, FileLogger>();
-            container.RegisterType<ILogger, DBLogger>();
-
+            var container = RegisterServices();
             var employee = container.Resolve<Employee>();
             employee.Save(employee);
 
             Console.ReadKey();
+        }
+
+        public static IUnityContainer RegisterServices()
+        {
+            //This config should come from appSetting but for simplitcy i have added here 
+            bool isFileLoggingEnabled = false;
+
+            IUnityContainer container = new UnityContainer();
+            if (isFileLoggingEnabled)
+                container.RegisterType<ILogger, FileLogger>();
+            else
+            {
+                container.RegisterType<ILogger, DBLogger>();
+            }
+
+            return container;
         }
     }
 }
